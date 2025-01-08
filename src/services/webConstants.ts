d6a9ec963b9bac431d01d3fbac2322e8bc9467b5
chiranjivDev <chiranjivmohanty@resiliencesoft.com>
2025-01-08 19:40:36 +0530

    initial commit

---
+++ src/services/webConstants.ts
@@ -0,0 +1,14 @@
+export const API_URL = {
+  REGISTER: 'users',
+  LOGIN: 'auth/login',
+  USERS: 'users',
+  AUTH: 'auth/login',
+
+  // Sender
+  PACKAGES: 'packages',
+  PACKAGE_CATEGORIES: 'packages/categories-and-subcategories',
+
+  // Traveler trips/packages
+  TRIPS: '',
+  TRAVELER_PACKAGE: 'traveler-package',
+};
